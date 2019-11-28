dc00d8f956e9da52e168cfa2756fce4f09478eeb
Angela Velasco Perea <angela.velascoo24@gmail.com>
2019-11-28 09:57:22 +0100

    Num

--- src/basicosAngela/Basicos.java
+++ src/basicosAngela/Basicos.java
@@ -17,6 +17,11 @@ public class Basicos {
         //Ejemplos de asignación de valores a las variables de tipos básicos
         entero = 10;
         doble = 5.6;
+        elMasPeque=0;
+        chiquitin=0;
+        duplicaEntero=0;
+        grande=0;
+        
 
         //Mostrar en pantalla el contendido de las variables de tipos básicos.
         System.err.println("Valor entero");
@@ -27,8 +32,6 @@ public class Basicos {
 
     public static void main (String [] args) {
     	numericos(); 
-    	Cadenas();
-    	Logicos();
     }
 }
 
